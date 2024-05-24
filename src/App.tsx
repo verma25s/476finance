45a27dda9cf66c2f5a90b507297ac386623f5777
Sahil <vsahil71@gmail.com>
2024-05-23 19:27:35 -0700

    Version 1

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { 
+  BrowserRouter as Router,
+   Routes, 
+   Route, 
+  } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+<Router>
+  <Routes>
+    <Route>
+
+      
+    </Route>
+
+  </Routes>
+  
+  </Router>     
+    </div>
+  );
+}
+
+export default App;
