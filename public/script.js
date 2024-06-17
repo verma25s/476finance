bf4677e65720addd7d0814c9f42f006e8aacb175
TechWizTejas <pateltejas0601@icloud.com>
2024-06-17 11:50:03 -0600

    Adding Script,js for animations
    Adding Animations on webpage

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            navLinks.forEach(nav => nav.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
