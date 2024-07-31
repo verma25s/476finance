9c8592570ba229bed21377c367d1cc97595f635f
Jwalant-Patel <pateljwalant0009@gmail.com>
2024-07-31 01:29:17 -0600

    CommentAdd

--- src/components/importImages.ts
+++ src/components/importImages.ts
@@ -1,7 +1,9 @@
+// function defination 
 function importAll(requireContext: { keys: () => string[]; (id: string): string }) {
+    // return an array   
     return requireContext.keys().map(requireContext);
 }
-
+// using the require.context method to get all image files (PNG, JPG, JPEG, SVG)  
 const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
-
+// export the array of imported images as the default export of the module
 export default images;
