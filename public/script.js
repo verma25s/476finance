08152f20752080b0336df6bfad27ff58822e3dde
Jwalant-Patel <pateljwalant0009@gmail.com>
2024-07-31 00:25:04 -0600

    ScriptCommentADD

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
-
+// JS event Listner
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('nav ul li a');
-
+    // stores them in a NodeList named navLinks
     navLinks.forEach(link => {
+        // for each link, it executes the provided callback function
         link.addEventListener('click', function () {
+            // adds an event listener to the current link
+            // when link clicked callback fn inside gets executed
             navLinks.forEach(nav => nav.classList.remove('active'));
+            //ensuring that active class will be given only when link is clicked
             this.classList.add('active');
+            // adds the 'active' class to the currently clicked link
         });
     });
 });
